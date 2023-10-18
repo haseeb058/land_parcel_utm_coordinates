ee2e49db4fe0112a742b7489eefb0b900d9cca2f
haseeb058 <meharhaseeb058.com>
2023-10-18 07:30:03 +0500

    implement the distance

--- src/components/map/helper.js
+++ src/components/map/helper.js
@@ -51,10 +51,32 @@ export const useMap = () => {
     }
   };
 
+  const CalculateDistance = (lat1, lon1, lat2, lon2) => {
+    const earthRadius = 6371; // Radius of the Earth in kilometers
+
+    const lat1Rad = (lat1 * Math.PI) / 180;
+    const lon1Rad = (lon1 * Math.PI) / 180;
+    const lat2Rad = (lat2 * Math.PI) / 180;
+    const lon2Rad = (lon2 * Math.PI) / 180;
+
+    const dLat = lat2Rad - lat1Rad;
+    const dLon = lon2Rad - lon1Rad;
+
+    const a =
+      Math.sin(dLat / 2) ** 2 +
+      Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = earthRadius * c; // Distance in kilometers
+
+    return distance;
+  };
+
   return {
     setCenter,
     onOverlayComplete,
     onLoadMap,
+    CalculateDistance,
     polygonOptions,
     containerStyle,
     isLoaded,
--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,37 +1,75 @@
 import React from "react";
-import { GoogleMap, Polygon } from "@react-google-maps/api";
+import { GoogleMap, Marker, Polygon } from "@react-google-maps/api";
 
 import { useMap } from "./helper";
 
 const MapComponent = () => {
   const { center, isLoaded, containerStyle } = useMap();
+  const polygon = [
+    { lat: 3.1439, lng: 101.6869 }, // Kuala Lumpur
+    { lat: 3.1538, lng: 101.7121 }, // Petaling Jaya
+    { lat: 3.5649, lng: 101.6005 }, // Shah Alam
+    { lat: 2.8264, lng: 101.6964 }, // Klang
+  ];
+
+  const onLoad = (polygon) => {
+    // Access the google.maps object
+    const { google } = window;
+
+    for (let i = 0; i < polygon.getPath().getLength(); i++) {
+      const pointA = polygon.getPath().getAt(i);
+      const pointB = polygon
+        .getPath()
+        .getAt((i + 1) % polygon.getPath().getLength());
+
+      const distance = google?.maps?.geometry?.spherical.computeDistanceBetween(
+        pointA,
+        pointB
+      );
+      const midpoint = google?.maps?.geometry?.spherical?.interpolate(
+        pointA,
+        pointB,
+        0.5
+      );
+
+      // Display the distance in the middle of each line
+      new google.maps.InfoWindow({
+        content: `Distance: ${distance.toFixed(2)} meters`,
+        position: midpoint,
+      }).open(polygon.getMap());
+    }
+  };
 
   return isLoaded ? (
-    <GoogleMap zoom={4} center={center} mapContainerStyle={containerStyle}>
-      <Polygon
-        options={{
-          strokeColor: "#FF0000",
-          strokeWeight: 2,
-        }}
-        paths={[
-          { lat: 3.139, lng: 101.6869 }, // Kuala Lumpur
-          { lat: 3.0538, lng: 101.7121 }, // Petaling Jaya
-          { lat: 3.0649, lng: 101.6005 }, // Shah Alam
-          { lat: 2.9264, lng: 101.6964 }, // Klang
-        ]}
-      />
-      <Polygon
-        options={{
-          strokeColor: "#FF0000",
-          strokeWeight: 2,
-        }}
-        paths={[
-          { lat: 3.1439, lng: 101.6869 }, // Kuala Lumpur
-          { lat: 3.1538, lng: 101.7121 }, // Petaling Jaya
-          { lat: 3.5649, lng: 101.6005 }, // Shah Alam
-          { lat: 2.8264, lng: 101.6964 }, // Klang
-        ]}
-      />
+    <GoogleMap zoom={11} center={center} mapContainerStyle={containerStyle}>
+      <Polygon onLoad={onLoad} paths={polygon} />
+      {/* {polygons.map((polygon, index) => {
+        for (let i = 0; i < polygon.length - 1; i++) {
+          const coord1 = polygon[i];
+          const coord2 = polygon[i + 1];
+
+          const midpoint = {
+            lat: (coord1.lat + coord2.lat) / 2,
+            lng: (coord1.lng + coord2.lng) / 2,
+          };
+
+          const distance = CalculateDistance(
+            coord1.lat,
+            coord1.lng,
+            coord2.lat,
+            coord2.lng
+          );
+
+          return (
+            <Marker
+              key={`marker-${index}-${i}`}
+              position={midpoint}
+              label={`${distance.toFixed(2)} km`}
+            />
+          );
+        }
+        return null;
+      })} */}
     </GoogleMap>
   ) : null;
 };
