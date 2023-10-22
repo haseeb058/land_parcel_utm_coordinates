4e906371a46dc33664fb8f88d47e16b104a79464
haseeb058 <meharhaseeb058.com>
2023-10-22 11:45:59 +0500

    section are removed

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -10,36 +10,36 @@ const MapComponent = ({ polygon, center, zoom, mapView, isLoaded }) => {
     borderRadius: "10px",
   };
 
-  const onLoad = (onLoadPolygon) => {
-    // Access the google.maps object
+  // const onLoad = (onLoadPolygon) => {
+  //   // Access the google.maps object
 
-    const { google } = window;
+  //   const { google } = window;
 
-    const windows = [];
+  //   const windows = [];
 
-    for (let i = 0; i < onLoadPolygon.getPath()?.getLength(); i++) {
-      const pointA = onLoadPolygon.getPath().getAt(i);
-      const pointB = onLoadPolygon
-        .getPath()
-        .getAt((i + 1) % onLoadPolygon.getPath().getLength());
-      const distance = google.maps.geometry?.spherical.computeDistanceBetween(
-        pointA,
-        pointB
-      );
-      const midpoint = google.maps.geometry?.spherical.interpolate(
-        pointA,
-        pointB,
-        0.5
-      );
+  //   for (let i = 0; i < onLoadPolygon.getPath()?.getLength(); i++) {
+  //     const pointA = onLoadPolygon.getPath().getAt(i);
+  //     const pointB = onLoadPolygon
+  //       .getPath()
+  //       .getAt((i + 1) % onLoadPolygon.getPath().getLength());
+  //     const distance = google.maps.geometry?.spherical.computeDistanceBetween(
+  //       pointA,
+  //       pointB
+  //     );
+  //     const midpoint = google.maps.geometry?.spherical.interpolate(
+  //       pointA,
+  //       pointB,
+  //       0.5
+  //     );
 
-      windows.push({
-        content: `${distance?.toFixed(2)} m`,
-        position: midpoint,
-      });
-    }
+  //     windows.push({
+  //       content: `${distance?.toFixed(2)} m`,
+  //       position: midpoint,
+  //     });
+  //   }
 
-    setInfoWindows(windows);
-  };
+  //   setInfoWindows(windows);
+  // };
 
   return isLoaded ? (
     <GoogleMap
@@ -50,27 +50,27 @@ const MapComponent = ({ polygon, center, zoom, mapView, isLoaded }) => {
         mapTypeId: mapView,
       }}
     >
-      {polygon.length > 1 && (
-        <Polygon
-          paths={polygon}
-          onLoad={onLoad}
-          options={{
-            strokeOpacity: 0.8,
-            strokeColor: "#FFD580",
-            fillColor: "#FFA500",
-          }}
-        />
-      )}
-      {InfoWindows.length > 1 &&
-        InfoWindows?.map((infoWindow, index) => (
-          <InfoWindow
-            key={`infowindow-${infoWindow.content}-${index}`}
-            position={infoWindow.position}
-            visible={false}
-          >
-            <div>{infoWindow.content}</div>
-          </InfoWindow>
-        ))}
+      {/* {polygon.length > 1 && ( */}
+      <Polygon
+        paths={polygon}
+        // onLoad={onLoad}
+        options={{
+          strokeOpacity: 0.8,
+          strokeColor: "#FFD580",
+          fillColor: "#FFA500",
+        }}
+      />
+      {/* )} 
+      // {InfoWindows.length > 1 &&
+      //   InfoWindows?.map((infoWindow, index) => (
+      //     <InfoWindow
+      //       key={`infowindow-${infoWindow.content}-${index}`}
+      //       position={infoWindow.position}
+      //       visible={false}
+      //     >
+      //       <div>{infoWindow.content}</div>
+      //     </InfoWindow>
+      //   ))}*/}
     </GoogleMap>
   ) : null;
 };
